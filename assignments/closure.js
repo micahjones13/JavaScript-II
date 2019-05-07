4fc6258c6452d87a45ee9d1435a64833c4d574c3
micahjones13 <micahjones13@gmail.com>
2019-05-07 15:03:54 -0600

    finished MVP and 1 closure stretch goal

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,24 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function kingdom() {
+  const hero = 'Link'
 
+  console.log(`Hail ${hero}`)
+
+  region(); //must invoke here to get data from region()
+
+  function region() {
+    const regionName = 'Hyrule'
+    console.log(`${hero}, From ${regionName}`)
+    city(); //must invoke here to get data from city()
+    function city() {
+      const cityName = 'Lost Woods'
+      console.log(`${hero}, From ${regionName}, From ${cityName}`)
+    }
+  }
+  
+}
+console.log(kingdom());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,10 +26,18 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function (){
+    count++;
+  }
+
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter(); 
+console.log(newCounter()); //1 
+console.log(newCounter()); //2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
